3181f9dd1a37e3b3e90f0dee2a2fc9c7e8d88749
shuhei-T <nofuture742617000027@gmail.com>
2022-01-12 23:21:58 +0900

    [Add] コントローラにフラッシュメッセージを追記 #26

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -4,8 +4,9 @@ class UserSessionsController < ApplicationController
   def create
     @user = login(params[:email], params[:password])
     if @user
-      redirect_back_or_to root_path
+      redirect_back_or_to root_path, success: t('.success')
     else
+      flash.now[:danger] = t('.fail')
       render :new
     end
   end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,10 +5,10 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-    # binding.pry
     if @user.save
-      redirect_to login_path
+      redirect_to login_path, success: t('.success')
     else
+      flash.now[:danger] = t('.fail')
       render :new
     end
   end
