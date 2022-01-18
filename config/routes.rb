7cf31f0d2afb68753b83f01791efcd29ab1ebbbe
shuhei-T <nofuture742617000027@gmail.com>
2022-01-18 18:47:34 +0900

    [Add] userプロフィール作成 #19

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,6 @@ Rails.application.routes.draw do
   delete 'logout', to: 'user_sessions#destroy'
 
   resources :users, only: %i[new create]
+  resource :profile, only: %i[show edit update]
   resources :reptiles
 end
