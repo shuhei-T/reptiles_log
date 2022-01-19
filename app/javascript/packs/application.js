8bf4c459c8594a0f05857263674684334dfc9f0d
shuhei-T <nofuture742617000027@gmail.com>
2022-01-19 22:12:18 +0900

    [Improve] プレビュー機能の実装を、ビューファイルに書く

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,19 +2,16 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
-import '@fortawesome/fontawesome-free/js/all';
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import "bootstrap";
-import "../stylesheets/application.scss";
 import '@fortawesome/fontawesome-free/js/all';
+import "../stylesheets/application.scss";
+import "jquery";
+import "./reptile.js"
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
-
-
-import "./reptile.js"
-import "./common.js"
