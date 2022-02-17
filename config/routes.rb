a7360fccdf1c78b94c834d506ba7d26211ad6a94
shuhei-T <nofuture742617000027@gmail.com>
2022-02-17 16:20:06 +0900

    [Add] プライバシーポリシー、利用規約作成

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   get 'login', to: 'user_sessions#new'
   post 'login', to: 'user_sessions#create'
   delete 'logout', to: 'user_sessions#destroy'
+  get 'privacy', to: 'static_pages#privacy'
+  get 'service', to: 'static_pages#service'
 
   resources :users, only: %i[new create index show] do
     collection do
