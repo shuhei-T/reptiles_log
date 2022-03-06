3d38dec3ef2e184e82919f0c89bcc971ca368645
shuhei-T <nofuture742617000027@gmail.com>
2022-03-06 18:35:39 +0900

    [Add] サイトマップ設定 103

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -1,18 +1,17 @@
-# require 'fog-aws'
-# # Set the host name for URL creation
-# SitemapGenerator::Sitemap.default_host = "https://www.reptileslog.com/"
-# # inform the map cross-linking where to find the other maps
-# SitemapGenerator::Sitemap.sitemaps_host = "https://s3-#{ENV['AWS_DEFAULT_REGION']}.amazonaws.com/#{ENV['AWS_S3_BADGET_NAME']}"
-# # pick a place safe to write the files
-# SitemapGenerator::Sitemap.public_path = 'tmp/'
-# # pick a namespace within your bucket to organize your maps
-# SitemapGenerator::Sitemap.sitemaps_path = 'sitemaps/'
-# # store on S3 using Fog (pass in configuration values as shown above if needed)
-# SitemapGenerator::Sitemap.adapter = SitemapGenerator::S3Adapter.new(fog_provider: 'AWS',
-#                                       aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
-#                                       aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
-#                                       fog_directory: ENV['AWS_S3_BADGET_NAME'],
-#                                       aws_region: ENV['AWS_DEFAULT_REGION'])
+# Set the host name for URL creation
+SitemapGenerator::Sitemap.default_host = "https://www.reptileslog.com/"
+# inform the map cross-linking where to find the other maps
+SitemapGenerator::Sitemap.sitemaps_host = "https://s3-#{ENV['AWS_DEFAULT_REGION']}.amazonaws.com/#{ENV['AWS_S3_BADGET_NAME']}/"
+# pick a place safe to write the files
+SitemapGenerator::Sitemap.public_path = 'tmp/'
+# pick a namespace within your bucket to organize your maps
+SitemapGenerator::Sitemap.sitemaps_path = 'sitemaps/'
+# store on S3 using Fog (pass in configuration values as shown above if needed)
+SitemapGenerator::Sitemap.adapter = SitemapGenerator::S3Adapter.new(fog_provider: 'AWS',
+                                      aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+                                      aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+                                      fog_directory: ENV['AWS_S3_BADGET_NAME'],
+                                      aws_region: ENV['AWS_DEFAULT_REGION'])
 
 SitemapGenerator::Sitemap.create do
   # Put links creation logic here.
