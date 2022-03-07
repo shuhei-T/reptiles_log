03ddd976c5d0558ed4f342ad1b1a4551d5a1c7da
shuhei-T <nofuture742617000027@gmail.com>
2022-03-08 00:53:57 +0900

    [Add] 祝日API導入 #55

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,6 +1,20 @@
 const { environment } = require('@rails/webpacker')
-
 const webpack = require('webpack')
+const dotenv = require('dotenv')
+
+const dotenvFiles = [
+    '.env'
+]
+dotenvFiles.forEach((dotenvFile) => {
+    dotenv.config({path: dotenvFile, silent: true})
+})
+
+environment.plugins.prepend('Environment',
+    new webpack.EnvironmentPlugin(
+        JSON.parse(JSON.stringify(process.env))
+    )
+)
+
 environment.plugins.prepend('Provide',
     new webpack.ProvidePlugin({
         $: 'jquery/src/jquery',
