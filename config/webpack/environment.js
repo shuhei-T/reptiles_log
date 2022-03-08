445241a6e9cd3ca3b4b072ea7333e6325001524e
shuhei-T <nofuture742617000027@gmail.com>
2022-03-08 14:12:24 +0900

    [Fix] yarn remove dotenvを実行 #111 config/webpack/environment.js を変更

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,13 +1,5 @@
 const { environment } = require('@rails/webpacker')
 const webpack = require('webpack')
-const dotenv = require('dotenv')
-
-const dotenvFiles = [
-    '.env'
-]
-dotenvFiles.forEach((dotenvFile) => {
-    dotenv.config({path: dotenvFile, silent: true})
-})
 
 environment.plugins.prepend('Environment',
     new webpack.EnvironmentPlugin(
