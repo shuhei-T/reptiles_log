3ced0cd86b6d4e889282cfd43d5c84867c3775bf
shuhei-T <nofuture742617000027@gmail.com>
2022-03-27 02:30:16 +0900

    [Add] グラフ改善 #56

--- app/controllers/charts_controller.rb
+++ app/controllers/charts_controller.rb
@@ -1,27 +1,27 @@
 class ChartsController < ApplicationController
+  before_action :set_reptile_id, only: %i[index]
 
   def index
-    @reptile = current_user.reptiles.find(params[:reptile_id])
-    @charts = @reptile.logs.all.order(created_at: :asc)
+    @charts = @reptile.logs.all.order(logged_at: :asc)
 
-    this_month = Date.today.all_month
+    # this_month = Date.today.all_month
 
 
 
     # 日付
-    day = []
+    day = Array.new
     @charts.each do |chart|
-      day << chart.created_at.strftime("%Y年%m月%d日 %H:%M").to_s
+      day << chart.logged_at.strftime("%Y年%m月%d日 %H:%M").to_s
     end
-    gon.day = day
+    @day = day.to_json.html_safe
 
     # 今月
-    month = []
-    @charts.each do |chart|
-      if (this_month.include?(Date.parse(chart[:created_at].to_s)))
-        month << chart.created_at.strftime("%Y年%m月%d日 %H:%M").to_s
-      end
-    end
+    # month = []
+    # @charts.each do |chart|
+    #   if (this_month.include?(Date.parse(chart[:created_at].to_s)))
+    #     month << chart.created_at.strftime("%Y年%m月%d日 %H:%M").to_s
+    #   end
+    # end
     # => ["2022年03月01日 02:58", "2022年03月01日 12:35", "2022年03月01日 13:16", "2022年03月01日 13:19", "2022年03月01日 13:22", "2022
     #   03月01日 16:09"]
 
@@ -31,16 +31,45 @@ class ChartsController < ApplicationController
     # end
 
     # 体重
-    gon.weight = @reptile.logs.pluck(:weight)
+    @weight = @reptile.logs.pluck(:weight).compact.to_json.html_safe
+    @weight_charts = @reptile.logs.where("weight IS NOT NULL").order(logged_at: :asc)
+    weight_day = Array.new
+    @weight_charts.each do |chart|
+      weight_day << chart.logged_at.strftime("%Y-%m-%d %H:%M").to_s
+    end
+    @weight_day = weight_day.to_json.html_safe
 
     # 体長
-    gon.length = @reptile.logs.pluck(:length)
+    @length = @reptile.logs.pluck(:length).compact.to_json.html_safe
+    @length_charts = @reptile.logs.where("length IS NOT NULL").order(logged_at: :asc)
+    length_day = Array.new
+    @length_charts.each do |chart|
+      length_day << chart.logged_at.strftime("%Y-%m-%d %H:%M").to_s
+    end
+    @length_day = weight_day.to_json.html_safe
 
     # 温度
-    gon.temperature = @reptile.logs.pluck(:temperature)
+    @temperature = @reptile.logs.pluck(:temperature).compact.to_json.html_safe
+    @temperature_charts = @reptile.logs.where("temperature IS NOT NULL").order(logged_at: :asc)
+    temperature_day = Array.new
+    @temperature_charts.each do |chart|
+      temperature_day << chart.logged_at.strftime("%Y-%m-%d %H:%M").to_s
+    end
+    @temperature_day = temperature_day.to_json.html_safe
 
     # 湿度
-    gon.humidity = @reptile.logs.pluck(:humidity)
+    @humidity = @reptile.logs.pluck(:humidity).compact.to_json.html_safe
+    @humidity_charts = @reptile.logs.where("humidity IS NOT NULL").order(logged_at: :asc)
+    humidity_day = Array.new
+    @humidity_charts.each do |chart|
+      humidity_day << chart.logged_at.strftime("%Y-%m-%d %H:%M").to_s
+    end
+    @humidity_day = humidity_day.to_json.html_safe
+  end
 
+  private
+
+  def set_reptile_id
+    @reptile = current_user.reptiles.find(params[:reptile_id])
   end
 end
