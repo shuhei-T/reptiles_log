fe1d46d773e17957eab2fe7a7317deea60d92ae6
shuhei-T <nofuture742617000027@gmail.com>
2022-03-29 23:48:12 +0900

    [Add] メイラー作成 #75

---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,9 @@
+class UserMailer < ApplicationMailer
+  
+  default from: ENV['MAIL_ADDRESS']
+  def reset_password_email(user)
+    @user = User.find(user.id)
+    @url = edit_password_reset_url(@user.reset_password_token)
+    mail(to: user.email, subject: t('defaults.password_reset'))
+  end
+end
