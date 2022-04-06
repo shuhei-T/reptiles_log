23288e4b235f151cb4a5d86cbcb0a59e9576d1f4
shuhei-T <nofuture742617000027@gmail.com>
2022-04-07 02:20:21 +0900

    [Fix] バグ対応

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,7 @@ class User < ApplicationRecord
   validates :name, presence: true, length: { maximum: 30 }
 
   has_many :reptiles, dependent: :destroy
+  has_many :logs
   has_many :active_relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
   has_many :passive_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
   has_many :following, through: :active_relationships, source: :followed
